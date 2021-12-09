0d59770f7edd770a6f232811c3a8a5b355d1df26
abdulmalek zain <lukezain@gmail.com>
2021-12-09 23:39:24 +0200

    init implementation - installed CRA, bootstrap, axios - basic implementation of requirements - added basic types
    todo:
      -performance fixes
      -dividing into components
      -implement tests
      -improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Emoji } from "./models/Emoji";
+import axios from "axios";
+import { IoSearch } from "react-icons/io5";
 
 function App() {
+  const [emojis, setEmojis] = useState<Emoji[]>();
+  const [search, setSearch] = useState("")
+  const [filteredEmojis, setFilteredEmojis] = useState<Emoji[]>()
+  useEffect(() => {
+    axios
+      .get(
+        `https://raw.githubusercontent.com/SalesChamp/emoji-list/main/emoji-list.json`
+      )
+      .then((response) => {
+        setEmojis(response.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    if(search) {
+      var filtered = emojis?.filter((emoji) => {
+        let keywords = emoji.keywords.split(" ")
+        let match = keywords.find(element => element.includes(search))
+        return match && match.length > 0
+      })
+      setFilteredEmojis(filtered)
+    } else {
+      setFilteredEmojis(undefined)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="headerSection">
+        <h2>SalesChamp Coding Challenge</h2>
       </header>
+      <section className="mainSection">
+        <div className="searchBar">
+          <div className="searchBox">
+            <IoSearch/>
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="searchField"
+              autoComplete="off"
+              type="text"
+              name="search"
+              placeholder="Search for an emoji using a keyword"
+            />
+          </div>
+        </div>
+        <section className="emojisSection">
+          <div className="row">
+            {filteredEmojis ?
+              filteredEmojis.map((emoji) => {
+                return (
+                  <div title={emoji.title} key={emoji.title} className="col-4 col-md-3 col-lg-2 emoji">
+                    {emoji.symbol}
+                  </div>
+                );
+              })
+              :
+            emojis ?
+              emojis.map((emoji) => {
+                return (
+                  <div title={emoji.title} key={emoji.title} className="col-4 col-md-3 col-lg-2 emoji">
+                    {emoji.symbol}
+                  </div>
+                );
+              })
+            : ""
+            }
+          </div>
+        </section>
+      </section>
     </div>
   );
 }
